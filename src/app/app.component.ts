981b02291f4f49696649ae96751e3f1de7dcb784
Brandon Tucker <brandon.tucker@adtran.com>
2018-07-05 20:08:50 +0000

    Add highlight to nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private router: Router) { }
+
   title = 'app';
+
+  /*navToDash(callback) {
+    this.router.navigate(['/dashboard']);
+    callback();
+  }
+  cScroll(x, y) {
+    window.scrollTo({left: x, top: y, behavior: 'smooth'});
+  }*/
+
+  navToLeaders() {
+    this.router.navigate(['/dashboard']);
+    window.scrollTo({left: 0, top: 0, behavior: 'smooth'});
+  }
+
+  navToActivities() {
+    this.router.navigate(['/dashboard']);
+    window.scrollTo({left: 0, top: 772-50, behavior: 'smooth'});
+  }
 }
-
-
