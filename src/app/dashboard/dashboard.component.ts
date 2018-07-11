321f7483c15b13622419c3ab08ea45ff8bd53c97
Brandon Tucker <brandon.tucker@adtran.com>
2018-07-11 21:57:06 +0000

    Cleanup

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import { ActivitiesComponent } from './activities/activities.component';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
